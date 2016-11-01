6d11c8d798ae7bace25eb92939e2f62e547fbfbe
Ben Van Sickle <email": null>
2016-11-01 11:36:18 -0500

    format linked list rspec

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,6 +1,6 @@
-require_relative "linked_list"
+require_relative "../linked_list"
 
-describe "Linked List" do
+RSpec.describe LinkedList do
 
   let(:l) {LinkedList.new}
 
@@ -24,81 +24,87 @@ describe "Linked List" do
     list_elements_backwards(nodes.link_back, node_elements)
   end
 
-  it "Inserts at the begining of the list" do
-    set_nodes
-    l.insert_first(-1)
-    expect(l.first.element).to eq(-1)
+  describe "First Node" do
+    it "Inserts at the begining" do
+      set_nodes
+      l.insert_first(-1)
+      expect(l.first.element).to eq(-1)
+    end
+
+    it "Removes the first node from the list" do
+      set_nodes
+      l.remove_first
+      expect(l.first.element).to eq 1
+    end
+
+    it "Returns nil if there are no nodes" do
+      expect(l.remove_first).to eq nil
+    end
   end
 
-  it "Removes the first node from the list" do
-    set_nodes
-    l.remove_first
-    expect(l.first.element).to eq 1
+  describe "Last Node" do
+    it "Inserts at end of list" do
+      set_nodes
+      l.insert_last(5)
+      new_list = list_elements(l.first)
+      expect(new_list).to eq [0,1,2,3,4,5]
+    end
+
+    it "Removes the last node" do
+      set_nodes
+      l.remove_last
+      new_list = list_elements(l.first)
+      expect(new_list).to eq [0,1,2,3]
+    end
+
+    it "Returns nil if there are no nodes" do
+      expect(l.remove_last).to eq nil
+    end
+
+    it "Links back properly" do
+      set_nodes
+      l.insert_last(5)
+      new_list = list_elements_backwards(l.last_node)
+      expect(new_list).to eq [5,4,3,2,1,0]
+    end
   end
 
-  it "Returns nil if there are no nodes" do
-    expect(l.remove_first).to eq nil
+  describe "Set" do
+    it "Changes the element of a given index" do
+      set_nodes
+      l.set(2, 10).element
+      new_list = list_elements(l.first)
+      expect(new_list).to eq [0,1,10,3,4]
+    end
   end
 
-  it "Inserts at end of list" do
-    set_nodes
-    l.insert_last(5)
-    new_list = list_elements(l.first)
-    expect(new_list).to eq [0,1,2,3,4,5]
+  describe "Get" do
+    it "Returns the node at a given index" do
+      set_nodes
+      expect(l.get(2).element).to eq 2
+    end
   end
 
-  it "Links back properly" do
-    set_nodes
-    l.insert_last(5)
-    new_list = list_elements_backwards(l.last_node)
-    expect(new_list).to eq [5,4,3,2,1,0]
+  describe "Insert" do
+    it "Inserts at the given index" do
+      set_nodes
+      l.insert(2, 10)
+      new_list = list_elements(l.first)
+      expect(new_list).to eq [0,1,10,2,3,4]
+    end
+
+    it "Links back properly" do
+      set_nodes
+      l.insert(2, 10)
+      new_list = list_elements_backwards(l.last_node)
+      expect(new_list).to eq [4,3,2,10,1,0]
+    end
   end
 
-  it "Removes the last node" do
-    set_nodes
-    l.remove_last
-    new_list = list_elements(l.first)
-    expect(new_list).to eq [0,1,2,3]
-  end
-
-  it "Returns nil if there are no nodes" do
-    expect(l.remove_last).to eq nil
-  end
-
-  it "Returns the node of a given index" do
-    set_nodes
-    expect(l.get(2).element).to eq 2
-  end
-
-  it "Changes the element of a given index" do
-    set_nodes
-    l.set(2, 10).element
-    new_list = list_elements(l.first)
-    expect(new_list).to eq [0,1,10,3,4]
-  end
-
-  it "Inserts an element at the given index" do
-    set_nodes
-    l.insert(2, 10)
-    new_list = list_elements(l.first)
-    expect(new_list).to eq [0,1,10,2,3,4]
-  end
-
-  it "Links back properly after inserting" do
-    set_nodes
-    l.insert(2, 10)
-    new_list = list_elements_backwards(l.last_node)
-    expect(new_list).to eq [4,3,2,10,1,0]
-  end
-
-  it "Returns the length of the list" do
-    set_nodes
-    expect(l.size).to eq 5
-  end
-
-  it "Print out link back nodes" do
-    set_nodes
-    new_list = list_elements_backwards(l.last_node)
-    expect(new_list).to eq [4,3,2,1,0]
+  describe "Size" do
+    it "Returns the length of the list" do
+      set_nodes
+      expect(l.size).to eq 5
+    end
   end
 end
