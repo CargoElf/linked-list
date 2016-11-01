cadbfda3bb7c0da6194c9449ed9d1246580dad77
Ben Van Sickle <email": null>
2016-11-01 11:48:29 -0500

    Add tests for link back to node

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,24 +1,48 @@
 require_relative "../node"
 
-describe "Node" do
+RSpec.describe Node do
 
   let(:node) {Node.new(10)}
 
-  it "Has an element" do
-    expect(node.element).to be 10
+  describe "Attributes" do
+    it "Has an element" do
+      expect(node.element).to be 10
+    end
+
+    it "Has readable refrence" do
+      expect(node.reference).to be nil
+    end
+
+    it "Has readable link back" do
+      expect(node.link_back).to be nil
+    end
   end
 
-  it "Links to another node" do
-    a_node = Node.new(3)
-    node.insert_after(a_node)
-    expect(node.reference).to be a_node
-  end
+  describe "Node Links" do
+    it "Links to the next node" do
+      a_node = Node.new(3)
+      node.insert_after(a_node)
+      expect(node.reference).to be a_node
+    end
 
-  it "Can remove a link to another node" do
-    a_node = Node.new(3)
-    node.insert_after(a_node)
-    node.remove_after
-    expect(node.reference).to be nil
-  end
+    it "Links to the previous node" do
+      a_node = Node.new(3)
+      node.insert_before(a_node)
+      expect(node.link_back).to be a_node
+    end
 
+    it "Can remove link to next node" do
+      a_node = Node.new(3)
+      node.insert_after(a_node)
+      node.remove_after
+      expect(node.reference).to be nil
+    end
+
+    it "Can remove link to previous node" do
+      a_node = Node.new(3)
+      node.insert_before(a_node)
+      node.remove_before
+      expect(node.link_back).to be nil
+    end
+  end
 end
